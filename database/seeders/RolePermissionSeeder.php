eb185693c2eb96b945e7a803c283a7ef2b8d2e8d
radmv <radmvarc@gmail.com>
2024-08-08 11:29:50 +0700

    create_roleseeder_for_owner

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RolePermissionSeeder extends Seeder
 {
@@ -29,6 +31,37 @@ class RolePermissionSeeder extends Seeder
                 ]);
             }
 
-            
+            $teacherRole = Role::firstOrCreate([
+                'name' => 'teacher'
+            ]);
+            $teacherPermissions = [
+                'manage courses',
+
+                // other singular action
+                'subscribe course',
+            ];
+            $teacherRole->syncPermissions($teacherPermissions);
+
+            $studentRole = Role::firstOrCreate([
+                'name' => 'student'
+            ]);
+            $studentPermissions = [
+                'subscribe course'
+            ];
+            $studentRole->syncPermissions($studentPermissions);
+
+            $ownerRole = Role::firstOrCreate([
+                'name' => 'owner'
+            ]);
+
+            $user = User::create([
+                'name' => 'Owner Radm',
+                'email' => 'admin@radm.com',
+                'email_verified_at' => now(),
+                'Occupation' => 'Owner',
+                'Avatar' => 'images/default-avatar.png',
+                'password' => bcrypt('password'),
+            ]);
+            $user->assignRole($ownerRole);
     }
 }
